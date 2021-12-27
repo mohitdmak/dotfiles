b2e957c7db2226035612faca99fa0084569c4041
mohitdmak <mdmohitrc@gmail.com>
2021-12-27 12:50:42 +0530

    fix: treesitter external language installation issue

--- neovim/.config/nvim/lua/user/treesitter.lua
+++ neovim/.config/nvim/lua/user/treesitter.lua
@@ -4,7 +4,8 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  --ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "typescript", "python", "bash", "dockerfile", "html", "javascript", "java", "json", "lua", "python", "vim" },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "haskell" }, -- List of parsers to ignore installing
   autopairs = {
