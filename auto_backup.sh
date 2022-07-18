7440053641a0794f8b8a6ff491665757139f8ef9
mohitdmak <mdmohitrc@gmail.com>
2022-07-18 12:20:57 +0530

    add: regular backup

--- auto_backup.sh
+++ auto_backup.sh
@@ -6,66 +6,79 @@ CYAN='\033[0;36m'
 
 # Here scripts will arrive if they fail
 die(){
-  echo -e "${RED} AUTO BACKUP FAILED :"
+  echo -e "${RED}AUTO BACKUP FAILED :"
   echo "$*" 1>&2
+  notify-send -u critical "[SYSTEMD SERVICE ERROR] AutoBackup Failed: $*"
   # exit 1
 }
+
 # debug
-echo -e "${CYAN} [ENTER] Home @ mohitdmak > > >"
+echo -e "${CYAN}[ENTER] Home @ mohitdmak > > >"
 cd && pwd
 
 # add personal gitlab ssh key
 PERSONAL_GITLAB_SSH_KEY_LOCATION="$HOME/.ssh/id_personal_gitlab_ed25519"
-echo -e "${CYAN} [SETUP] Starting SSH-AGENT and adding Personal GitLab SSH Key > > >"
-eval "$(ssh-agent -s)"
-ssh-add ${PERSONAL_GITLAB_SSH_KEY_LOCATION} | 
+echo -e "${CYAN}[SETUP] Starting SSH-AGENT and adding Personal GitLab SSH Key > > >"
+if eval "$(ssh-agent -s)"
+then 
+    :
+else
+    die "[ERROR SETUP]:Could not start SSH-AGENT."
+fi
+if ssh-add ${PERSONAL_GITLAB_SSH_KEY_LOCATION} 
+then
+    :
+else
     die "[ERROR SETUP]:Could not add personal gitlab ssh key for regular backup."
-echo -e "${CYAN} [SETUP] SSH-AGENT and GitLab SSH Keys setup < < <"
+fi
+echo -e "${CYAN}[SETUP] SSH-AGENT and GitLab SSH Keys setup < < <"
 
 # function to enter repository provided by 1st arg, and send updates to upsream if any
 auto_backup () {
-    echo -e "${CYAN} [ENTER] SYSTEM CRITICAL GIT REPO @ ${1} > > >"
+    echo -e "${CYAN}[ENTER] SYSTEM CRITICAL GIT REPO @ ${1} > > >"
     cd "$1" && pwd
-    echo -e "${CYAN} [FETCH] Fetching from upstream : "
+    echo -e "${CYAN}[FETCH] Fetching from upstream : "
+    die "[ERROR ADD]:Could not add git diff @ ${1} !"
     if git fetch origin 
     then
-        continue
+        :
     else
         die "[ERROR FETCH]:Could not fetch upstream @ ${1} !"
     fi
     git status
     if [[ `git status --porcelain` ]]; then
-        echo -e "${CYAN} [ADD] Adding Hunks : "
+        echo -e "${CYAN}[ADD] Adding Hunks : "
         if git add . 
         then 
-            continue
+            :
         else 
             die "[ERROR ADD]:Could not add git diff @ ${1} !"
         fi
-        echo -e "${CYAN} [COMMIT] Committing Diffs : "
+        echo -e "${CYAN}[COMMIT] Committing Diffs : "
         if git commit -m "add: regular backup" 
         then 
-            continue
+            :
         else 
             die "[ERROR COMMIT]: Could not commit @ ${1} !"
         fi
-        echo -e "${CYAN} [PUSH] Pushing Update : "
+        echo -e "${CYAN}[PUSH] Pushing Update : "
         if git push 
         then 
-            continue
+            :
         else die "[ERROR PUSH]: Unable to Push @ ${1}; most probably an ssh-id agent inactive issue ! < < <"
         fi
     else
-        echo -e "${CYAN} [STATUS] No updates in ${1}"
+        echo -e "${CYAN}[STATUS] No updates in ${1}"
     fi
 }
 
 # system critical locations
 DOTFILES="$HOME/dotfiles"
-SEMTHREEONE="$HOME/vault/semester::3:1"
+SEM31="$HOME/vault/semester::3:1"
 SEM22="$HOME/vault/sem-2-2"
 
 # Start backing up . . .
+notify-send -u critical "[SYSTEMD SERVICE NOTIF]: @mohtidmak: Auto Backing up System Critical GIT Locations; Avoid ShutDown."
 auto_backup ${DOTFILES}
-auto_backup ${SEMTHREEONE}
+auto_backup ${SEM31}
 auto_backup ${SEM22}
