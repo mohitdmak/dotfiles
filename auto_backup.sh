f86b3febbe4eeab1ddf6cad5a982b8c2a957992b
mohitdmak <mdmohitrc@gmail.com>
2022-07-18 11:29:57 +0530

    add: regular backup

--- auto_backup.sh
+++ auto_backup.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
 
+# Utility vars
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+
 # Here scripts will arrive if they fail
 die(){
-  echo "\nFAILED"
+  echo -e "${RED} AUTO BACKUP FAILED :"
   echo "$*" 1>&2
   # exit 1
 }
 # debug
-echo "Entering Home @ mohitdmak > > >"
+echo -e "${CYAN} [ENTER] Home @ mohitdmak > > >"
 cd && pwd
 
 # add personal gitlab ssh key
-echo "Starting SSH-AGENT and adding Personal GitLab SSH Key > > >"
+echo -e "${CYAN} [SETUP] Starting SSH-AGENT and adding Personal GitLab SSH Key > > >"
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_personal_gitlab_ed25519 | die "ERROR [SETUP]:Could not add personal gitlab ssh key for regular backup."
-echo "SSH-AGENT and GitLab SSH Keys setup < < <"
+ssh-add ~/.ssh/id_personal_gitlab_ed25519 | 
+    die "ERROR [SETUP]:Could not add personal gitlab ssh key for regular backup."
+echo -e "${CYAN} [SETUP] SSH-AGENT and GitLab SSH Keys setup < < <"
 
 # dotfiles
-echo "Moving to Dotfiles @ ~/dotfiles > > >"
-cd ~/dotfiles && pwd
-git fetch origin
-git status
-echo "Adding Hunks : "
-git add .
-git commit -m "add: regular backup"
-echo "Pushing Update : "
-git push | die "ERROR: Unable to Push git diffs in ~/vault/semester::2:2; most probably an ssh-id agent inactive issue! < < <"
+auto_backup () {
+    echo -e "${CYAN} [ENTER] SYSTEM CRITICAL GIT REPO @ ${1} > > >"
+    cd $1 && pwd
+    echo -e "${CYAN} [FETCH] Fetching from upstream : "
+    git fetch origin | die "ERROR [FETCH]:Could not fetch upstream @ ${1} !"
+    git status
+    if [[ `git status --porcelain` ]]; then
+        echo -e "${CYAN} [ADD] Adding Hunks : "
+        git add . | die "ERROR [ADD]:Could not add git diff @ ${1} !"
+        echo -e "${CYAN} [COMMIT] Committing Diffs : "
+        git commit -m "add: regular backup" | die "ERROR [COMMIT]: Could not commit @ ${1} !"
+        echo -e "${CYAN} [PUSH] Pushing Update : "
+        git push | die "ERROR [PUSH]: Unable to Push @ ${1}; most probably an ssh-id agent inactive issue ! < < <"
+    else
+        echo -e "${CYAN} [STATUS] No updates in ${1}"
+    fi
+}
+dotfiles="$HOME/dotfiles"
+sem31="$HOME/vault/semester::3:1"
+sem22="$HOME/vault/sem-2-2"
+auto_backup dotfiles
+auto_backup sem31
+auto_backup sem22
 
 # vault - semester 3-1
 echo "Moving to Vault Semester::3:1 @ ~/vault/semester::3:1 > > >"
