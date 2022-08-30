ca3c32348f0b0af006926e805bbd7807da93afb6
mohitdmak <mdmohitrc@gmail.com>
2022-08-30 17:33:30 +0530

    add: regular backup

--- scripts/auto_backup.sh
+++ scripts/auto_backup.sh
@@ -79,6 +79,52 @@ auto_backup () {
     fi
 }
 
+# function to enter repository provided by 1st arg, and send/receive updates to upsream if any
+auto_backup_restore () {
+    echo -e "${CYAN}[ENTER] SYSTEM CRITICAL GIT REPO @ ${1} > > >"
+    cd "$1" && pwd
+    echo -e "${CYAN}[FETCH] Fetching from upstream : "
+    if git fetch origin 
+    then
+        git pull
+    else
+        die "[ERROR FETCH]:Could not fetch upstream @ ${1} !"
+    fi
+    git status
+    if [[ `git status --porcelain` ]]; then
+        echo -e "${CYAN}[ADD] Adding Hunks : "
+        if git add . 
+        then 
+            :
+        else 
+            die "[ERROR ADD]:Could not add git diff @ ${1} !"
+        fi
+        echo -e "${CYAN}[COMMIT] Committing Diffs : "
+        if git commit -m "add: regular backup" 
+        then 
+            :
+        else 
+            die "[ERROR COMMIT]: Could not commit @ ${1} !"
+        fi
+    # obtain branch and check for pending commits (due to ssh/network issues)
+    BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+    echo -e "${CYAN}[CHECK] Checking for pending commits @ ${BRANCH} @ origin : > > >"
+    PENDING_COMMITS=$(git log origin/${BRANCH}..${BRANCH} | cat)
+    if [[ ${PENDING_COMMITS} == "" ]]; then
+        echo -e "${CYAN}[CHECK] No pending commits < < <"
+    else
+        echo -e "${CYAN}[PUSH] Found pending commits; pushing updates : "
+        if git push 
+        then 
+            :
+        else die "[ERROR PUSH]: Unable to Push @ ${1}; most probably an ssh-id agent inactive issue ! < < <"
+        fi
+    fi
+    else
+        echo -e "${CYAN}[STATUS] No updates in ${1} < < <"
+    fi
+}
+
 # system critical locations
 DOTFILES="$HOME/dotfiles"
 SEM31="$HOME/vault/semester::3:1"
@@ -96,4 +142,4 @@ paplay "$SOUND_TO_PLAY" --volume $SOUND_VOLUME
 auto_backup ${DOTFILES}
 auto_backup ${SEM31}
 auto_backup ${SEM22}
-auto_backup ${ALGOS}
+auto_backup_restore ${ALGOS}
