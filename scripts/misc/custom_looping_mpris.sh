05246e130539d0baa4aa8ef062f2d5020d84d30d
mohitdmak <mdmohitrc@gmail.com>
2023-11-26 18:16:04 +0530

    add: regular backup

--- scripts/misc/custom_looping_mpris.sh
+++ scripts/misc/custom_looping_mpris.sh
@@ -4,15 +4,15 @@ LOOPING_NOTIF_ID=1
 
 if [[ $(playerctl --player=spotify loop | grep 'Track') == *Track* ]]; then
     playerctl --player=spotify loop playlist
-    LOOPING_NOTIF_ID=$(notify-send --urgency=low --app-name=Spotify -p "[@mohitdmak] Looping set to Playlist")
+    LOOPING_NOTIF_ID=$(notify-send --urgency=low --app-name=Spotify -p -r $LOOPING_NOTIF_ID "[@mohitdmak] Looping set to Playlist")
     # notify-send "${LOOPING_NOTIF_ID}"
 elif [[ $(playerctl --player=spotify loop | grep 'Playlist') == *Playlist* ]]; then
     playerctl --player=spotify loop track
-    LOOPING_NOTIF_ID=$(notify-send --urgency=low --app-name=Spotify -p "[@mohitdmak] Looping set to Track")
+    LOOPING_NOTIF_ID=$(notify-send --urgency=low --app-name=Spotify -p -r $LOOPING_NOTIF_ID "[@mohitdmak] Looping set to Track")
     # notify-send "${LOOPING_NOTIF_ID}"
 else
     playerctl --player=spotify loop track
-    LOOPING_NOTIF_ID=$(notify-send --urgency=low --app-name=Spotify -p "[@mohitdmak] Looping set to Track")
+    LOOPING_NOTIF_ID=$(notify-send --urgency=low --app-name=Spotify -p -r $LOOPING_NOTIF_ID "[@mohitdmak] Looping set to Track")
     # notify-send "${LOOPING_NOTIF_ID}"
 fi
 
