2c53405159eefb8c44227e71ff6509dda88b4fd7
mmakwana <mmakwana@cisco.com>
2024-06-27 00:09:21 +0530

    add: regular backup from mac intel

--- neovim/.config/nvim/lua/user/colorscheme.lua
+++ neovim/.config/nvim/lua/user/colorscheme.lua
@@ -72,8 +72,8 @@ require("tokyonight").setup({
 -- 2) GRUVBOX MATERIAL THEME (RUSTY) :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 -- TODO: GITSIGNS VERY BRIGHT HERE!
 vim.cmd[[
-    set background=dark
-    " set background=light
+    " set background=dark
+    set background=light
     let g:gruvbox_material_palette = 'material'
     let g:gruvbox_material_foreground = 'material'
     let g:gruvbox_material_background ='medium'
